d19a13897c8a5194c3aca07a24deaa60e8a16a54
kenboydz <kenboydz@gmail.com>
2019-06-11 20:46:05 +0800

    增加 map 的 load save 功能和测试；

--- richman/map.py
+++ richman/map.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*
 '''map
 '''
+import pickle
+import os
+
 from richman.place import BasePlace
 from richman.player import BasePlayer
 
 
-class PlaceAlreadyExistException(Exception):
+class PlaceAlreadyExistInMapException(Exception):
     def __init__(self, *args):
         super().__init__("该土地或项目已经存在！")
 
@@ -16,33 +19,125 @@ class PlayerAlreadyExistException(Exception):
 
 class BaseMap:
 
-    __places = []
-    __places_set = set()
+    __items = []
+    __items_set = set()
     
-    def __init__(self, name: str, places: list):
+    def __init__(self, name: str, items:list = []):
         '''init
 
         :param name: map name
-        :param places: places in the map
+        :param items: items in the map
         '''
         self.__name = name
-        self.__places = []
-        self.__places_set = set()
-        self._add_places_to_map(places)
+        self.__items = []
+        self.__items_set = set()
+        self._add_items_to_map(items)
 
     @property
     def name(self):
         return self.__name
     @property
-    def places(self):
-        return self.__places
+    def items(self):
+        return self.__items
 
-    def _add_places_to_map(self, places: list):
-        if places and not isinstance(places, list):
-            places = [places]
-        for place in places:
-            if not place.name in self.__places_set:
-                self.__places.append(place)
-                self.__places_set.add(place.name)
+    def _add_items_to_map(self, items: list):
+        if items and not isinstance(items, list):
+            items = [items]
+        for item in items:
+            if isinstance(item, PlaceEstate):
+                if not item.name in self.__items_set:
+                    self.__items.append(item)
+                    self.__items_set.add(item.name)
+                else:
+                    raise PlaceAlreadyExistInMapException()
             else:
-                raise PlaceAlreadyExistException()
+                self.__items.append(item)
+
+    def load(self, file_path: str):
+        '''load map from pickle
+
+        :param file_path: file_path to load
+        '''
+        assert os.path.exists(file_path),\
+            '用于读取 map 的文件不存在：{}。'.format(file_path)
+        with open(file_path, 'rb') as f:
+            map = pickle.load(f)
+        assert map, '读取或解析失败：{}。'
+        self.__items = map['items']
+        self.__name = map['name']
+
+    def save(self, file_path: str):
+        '''save map into pickle
+
+        :param file_path: file_path to save
+        '''
+        map = {}
+        map['items'] = self.items
+        map['name'] = self.name
+        with open(file_path, 'wb') as f:
+            pickle.dump(map, f)
+
+
+from richman.place import (
+    PlaceEstate,
+    PlaceEstateBlock,
+    PlaceProjectNuclear,
+    PlaceProjectBuilder,
+    PlaceProjectStation,
+    PlaceProjectTv,
+    PlaceProjectAirport,
+    PlaceProjectSewerage,
+    PlaceProjectSeaport
+)
+from richman.event import (
+    EventStart,
+    EventNews,
+    EventTrial,
+    EventLuck,
+    EventStock,
+    EventPrison,
+    EventPark,
+    EventTax
+)
+
+class MapSuperRichman(BaseMap):
+    '''超级地产富翁：地产大亨
+    '''
+    __blocks = []
+    __estates = []
+
+    def __init__(self):
+        self.__name == '超级地产富翁：地产大亨'
+        self._build()
+    
+    @property
+    def name(self):
+        return self.__name
+
+    def _build(self):
+        # setup estate block
+        self.__blocks = []
+        self.__blocks.append(PlaceEstateBlock('block0'))
+        self.__blocks.append(PlaceEstateBlock('block1'))
+        self.__blocks.append(PlaceEstateBlock('block2'))
+        self.__blocks.append(PlaceEstateBlock('block3'))
+        self.__blocks.append(PlaceEstateBlock('block4'))
+        self.__blocks.append(PlaceEstateBlock('block5'))
+        # setup estates
+        self.__estates = []
+        self.__estates.append(PlaceEstate(
+            name='沈阳',
+            fees=[400, 1000, 2500, 5500],
+            buy_value=2400,
+            pledge_value=1200,
+            upgrade_value=600,
+            block=self.__blocks[0]
+            ))
+        self.__estates.append(PlaceEstate(
+            name='天津',
+            fees=[500, 1100, 3000, 6000],
+            buy_value=2600,
+            pledge_value=1300,
+            upgrade_value=600,
+            block=self.__blocks[0]
+            ))
--- test/test_map.py
+++ test/test_map.py
@@ -5,30 +5,42 @@ from unittest.mock import MagicMock
 
 from richman.map import (
     BaseMap,
-    PlaceAlreadyExistException,
+    PlaceAlreadyExistInMapException,
     PlayerAlreadyExistException
 )
-from richman.place import BasePlace
+from richman.place import BasePlace, PlaceEstate, PlaceEstateBlock
 
 
 class TestBaseMap(unittest.TestCase):
 
     def setUp(self):
-        pass
-    
+        place1 = BasePlace('Hangzhou', 2200, 1100)
+        place2 = BasePlace('Xiamen', 3300, 2200)
+        place3 = BasePlace('Shanxi', 4400, 2200)
+        self.map = BaseMap('China', [place1, place2, place3])
+
     def tearDown(self):
         pass
 
     def test_add_to_map_should_add_places_and_players_to_map(self):
-        place1 = BasePlace('Hangzhou', 2200, 1100)
-        place2 = BasePlace('Xiamen', 3300, 2200)
-        place3 = BasePlace('Xiamen', 4400, 2200)
-        with self.assertRaises(PlaceAlreadyExistException):
+        block = PlaceEstateBlock('block1')
+        place1 = PlaceEstate('Hangzhou', [1,2,3,4], 2200, 1100, 1, block)
+        place2 = PlaceEstate('Xiamen', [1,2,3,4], 3300, 2200, 2, block)
+        place3 = PlaceEstate('Xiamen', [1,2,3,4], 4400, 2200, 3, block)
+        with self.assertRaises(PlaceAlreadyExistInMapException):
             map = BaseMap('China', [place1, place2, place3])
         map = BaseMap('China', [place1, place2])
         self.assertListEqual(
-            map.places,
+            map.items,
             [place1,
              place2]
         )
 
+    def test_save_and_load_map_should_operate_correctly(self):
+        self.map.save(r'./map_test.pickle')
+        map_new = BaseMap(name='China New')
+        map_new.load(r'./map_test.pickle')
+        self.assertEqual(self.map.name, map_new.name)
+        self.assertEqual(map_new.items[0].name, 'Hangzhou')
+        self.assertEqual(map_new.items[1].name, 'Xiamen')
+        self.assertEqual(map_new.items[2].name, 'Shanxi')
